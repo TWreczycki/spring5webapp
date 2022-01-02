6f0a95cd876c476c6bc47d5e5aefcef8edc3211a
TWreczycki <toreno94@gmail.com>
2022-01-02 22:19:14 +0100

    JPA 16. book fix

--- src/main/java/guru/springframework/spring5webapp/domain/Book.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Book.java
@@ -1,6 +1,7 @@
 package guru.springframework.spring5webapp.domain;
 
 import javax.persistence.*;
+import java.util.HashSet;
 import java.util.Set;
 
 @Entity
@@ -8,12 +9,12 @@ public class Book {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private long id;
+    private Long id;
     private String title;
     private String isbn;
 
     @ManyToMany
-    @JoinTable(name = "author_book", joinColumns = JoinColumn(name = "book_id"), inverseJoinColumns = JoinColumn(name = "author_id"))
+    @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"), inverseJoinColumns = @JoinColumn(name = "author_id"))
     private Set<Author> authors;
 
     public Book() {
