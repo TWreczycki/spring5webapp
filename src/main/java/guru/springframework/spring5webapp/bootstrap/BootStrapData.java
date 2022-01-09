8751af564d52e8be112494c4d2dc7a577569f3cd
TWreczycki <toreno94@gmail.com>
2022-01-09 19:39:02 +0100

    21. MVC BookController v0

--- src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -2,8 +2,10 @@ package guru.springframework.spring5webapp.bootstrap;
 
 import guru.springframework.spring5webapp.domain.Author;
 import guru.springframework.spring5webapp.domain.Book;
+import guru.springframework.spring5webapp.domain.Publisher;
 import guru.springframework.spring5webapp.repositories.AuthorRepository;
 import guru.springframework.spring5webapp.repositories.BookRepository;
+import guru.springframework.spring5webapp.repositories.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -12,10 +14,12 @@ public class BootStrapData implements CommandLineRunner {
 
     private final AuthorRepository authorRepository;
     private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
 
-    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
@@ -34,7 +38,27 @@ public class BootStrapData implements CommandLineRunner {
         rod.getBooks().add(noEJB);
         noEJB.getAuthors().add(rod);
 
+        authorRepository.save(rod);
+        bookRepository.save(noEJB);
+
+        Publisher myPub = new Publisher("MyPublisher", "Chrzaszczyrzeworzyce Powiat Lekolody", "Chrzaszczyrzewoszyce", "Lekolody", "20-200");
+
+        publisherRepository.save(myPub);
+
+        ddd.setPublisher(myPub);
+        myPub.getBooks().add(ddd);
+
+        publisherRepository.save(myPub);
+
+        noEJB.setPublisher(myPub);
+        myPub.getBooks().add(noEJB);
+
+        publisherRepository.save(myPub);
+
         System.out.println("Started in Bootstrap");
         System.out.println("Number of books: " + bookRepository.count());
+        System.out.println("Number of publishers: " + publisherRepository.count());
+        System.out.println("Publisher number of books: " + myPub.getBooks().size());
+
     }
 }
